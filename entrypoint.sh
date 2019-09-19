decae01d5ec663bc5cc365fda1913dbdb90d9ad9
adouglas-ics <adouglas@interface-cs.co.uk>
2019-09-19 11:14:44 +0100

    entrypoint sets output.result with addition

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,5 @@
 echo "First number to add is $1"
 echo "Second number to add is $2"
 result=$($1+$2)
-echo "$1 plus $2 equals $result"
+::set-output name=result::$result
+echo "$1 plus $2 equals output.result"

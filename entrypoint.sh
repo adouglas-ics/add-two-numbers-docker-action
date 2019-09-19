b4737ad962e4f5975f3fd473389510cef68e6c53
adouglas-ics <adouglas@interface-cs.co.uk>
2019-09-19 11:35:42 +0100

    further entrypoint edits

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@
 echo "First number to add is $1"
 echo "Second number to add is $2"
 result=expr $1 + $2
-::set-output name=result::$result
-echo "$1 plus $2 equals output.result"
+echo ::set-output name=result::$result
+echo "$1 plus $2 equals ${{outputs.result}}"

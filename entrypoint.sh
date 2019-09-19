6169d3120d11315f14edf564c952b75a700ddc52
adouglas-ics <adouglas@interface-cs.co.uk>
2019-09-19 11:57:08 +0100

    trying let variable declaration in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 
 echo "First number to add is $1"
 echo "Second number to add is $2"
-result=(expr $1 + $2)
+let result=$1+$2
 echo ::set-output name=result::$result
 echo "$1 plus $2 equals $result"
